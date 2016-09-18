6e8606d9757ae72f2961d765bedfe9779ee74cbc
jaeoheeail <joelfooet@gmail.com>
2016-09-18 18:30:24 +0800

    Add Storage.java interface

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -1,12 +1,12 @@
 package seedu.addressbook.storage;
 
 import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.InvalidStorageFilePathException;
 import seedu.addressbook.storage.StorageFile.StorageOperationException;
 
 public interface Storage {
-
-    abstract public String getPath();
+    abstract public StorageFile initializeStorage() throws InvalidStorageFilePathException;
     abstract public AddressBook load() throws StorageOperationException;
     abstract public void save(AddressBook addressBook) throws StorageOperationException;
-    abstract public StorageFile initializeStorage();
+    abstract public String getPath();
 }
