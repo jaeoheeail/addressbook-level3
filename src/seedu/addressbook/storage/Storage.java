3b77bd6ee2ee3a978b9fba1fec96aec8ed8e3ed9
jaeoheeail <joelfooet@gmail.com>
2016-09-18 18:48:17 +0800

    Remove exception

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -1,11 +1,11 @@
 package seedu.addressbook.storage;
 
 import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.storage.StorageFile.InvalidStorageFilePathException;
 import seedu.addressbook.storage.StorageFile.StorageOperationException;
 
 public interface Storage {
-    abstract public StorageFile initializeStorage() throws InvalidStorageFilePathException;
+    
+    
     abstract public AddressBook load() throws StorageOperationException;
     abstract public void save(AddressBook addressBook) throws StorageOperationException;
     abstract public String getPath();
